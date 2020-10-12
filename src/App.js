fae8bd39a0be580e921bf702af98e0d4fa09c1c3
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-10-12 13:35:00 -0400

    Added States

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [paragraphs, setParagraphs] = useState([]);
+  const [tag, setTag] = useState("p");
+  const [inputValue, setInputValue] = useState(1);
+  const [includeHtml, setIncludeHtml] = useState("Yes");
+  const [copiedCode, setCopiedCode] = useState(false);
+
   return (
     <div className="App">
       <h1>Welcome</h1>
     </div>
   );
-}
+};
 
 export default App;
